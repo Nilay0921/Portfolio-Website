0ffe525d49298cd0d0a72d465afe903d676586b3
Nilay0921 <61914014+Nilay0921@users.noreply.github.com>
2021-07-03 02:49:33 -0400

    Updated information

--- src/components/exp.js
+++ src/components/exp.js
@@ -7,43 +7,41 @@ function exp() {
             <div className="expirence-title">
                 <h1>Experience</h1>
             </div>
-            <div className="expirence1">
+            <div className="expirence1" onclick="window.location=http://google.com">
                 <div className="expirence1-title">
-                    <h1>Agincourt Robotics</h1>
-                    <h2>Programmer</h2>
+                    <a href="https://www.fispan.com/"><h1>FISPAN</h1></a>
+                    <h2>Software Engineering Intern</h2>
                 </div>
                 <div className="expirence1-p">
                     <ul>
-				        <li>Developed and debugged features using programming language C and git to provide functionality to the robot</li>
-				        <li>Communicated ideas proficiently with team members and leads</li>
-				        <li>Organized and maintained an engineering journal which kept track of accomplishments for each day during build season</li>
+                        <li>Joining FISPAN as a software engineer on the banking team</li>
 			        </ul>
                 </div>
             </div>
 
             <div className="expirence2">
                 <div className="expirence2-title">
-                    <h1>Agincourt Tutoring</h1>
-                    <h2>Math Tutor</h2>
+                    <h1>Agincourt Robotics</h1>
+                    <h2>Programmer</h2>
                 </div>
                 <div className="expirence2-p">
                     <ul>
-                        <li>Tutored grade 9, 10 and 11 students and gave them useful study tips</li>
-				        <li>Effectively communicated with students to complete challenging problems, and helped students gain intensive knowledge on mathematical concepts</li>
+                        <li>Developed and debugged features using programming language C and git to provide functionality to the robot</li>
+				        <li>Communicated ideas proficiently with team members and leads</li>
+				        <li>Organized and maintained an engineering journal which kept track of accomplishments for each day during build season</li>
 			        </ul>
                 </div>
             </div>
 
             <div className="expirence3">
                 <div className="expirence3-title">
-                    <h1>Agincourt Cricket Team</h1>
-                    <h2>Cricket Player and Captain</h2>
+                    <h1>Agincourt Tutoring</h1>
+                    <h2>Math Tutor</h2>
                 </div>
                 <div className="expirence3-p">
                     <ul>
-                        <li>Organized teammates and took initiative in coordinating team activities </li>
-				        <li>Lead a team of 15 to 2 championships and won the Most Valuable Player (MVP) Award</li>
-				        <li>Mentored new players and encouraged players to practice frequently</li>
+                        <li>Tutored grade 9, 10 and 11 students and gave them useful study tips</li>
+				        <li>Effectively communicated with students to complete challenging problems, and helped students gain intensive knowledge on mathematical concepts</li>
 			        </ul>
                 </div>
             </div>
