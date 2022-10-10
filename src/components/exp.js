99fcabe032088deedabf302c654d4261840696c0
Nilay0921 <61914014+Nilay0921@users.noreply.github.com>
2022-10-10 01:51:13 -0400

    Updated work history

--- src/components/exp.js
+++ src/components/exp.js
@@ -7,12 +7,12 @@ function exp() {
             <div className="expirence-title">
                 <h1>Experience</h1>
             </div>
-            <div className="expirence1" onclick="window.location=http://google.com">
-                <div className="expirence1-title">
+            <div className="expirence2" onclick="window.location=http://google.com">
+                <div className="expirence2-title">
                     <a href="https://www.fispan.com/"><h1>FISPAN</h1></a>
                     <h2>Software Developer Intern</h2>
                 </div>
-                <div className="expirence1-p">
+                <div className="expirence2-p">
                     <ul>
                         <li>Worked closely with large-scale microservices architecture to develop new invoice receivables features using Java and Spring impacting 200+ businesses and banks</li>
                         <li>Improved storage of account information by creating monthly partitions of the SQL table enhancing query processing by 30%</li>
@@ -22,29 +22,31 @@ function exp() {
                 </div>
             </div>
 
-            <div className="expirence2">
-                <div className="expirence2-title">
-                    <h1>Agincourt Robotics</h1>
-                    <h2>Programmer</h2>
+            <div className="expirence1" onclick="window.location=http://google.com">
+                <div className="expirence1-title">
+                <a href="https://www.meta.com/"><h1>Meta</h1></a>
+                    <h2>Software Engineering Intern</h2>
                 </div>
-                <div className="expirence2-p">
+                <div className="expirence1-p">
                     <ul>
-                        <li>Developed and debugged features using programming language C and git to provide functionality to the robot</li>
-				        <li>Communicated ideas proficiently with team members and leads</li>
-				        <li>Organized and maintained an engineering journal which kept track of accomplishments for each day during build season</li>
+                        <li>Developed and improved infrastructure to solve and remediate privacy problems using Hack/PHP</li>
+				        <li>Improved code file enumerations by providing instant updates for code file assets eliminating delays of up to 24 hours</li>
+				        <li>Added 100,000+ new undiscovered code file assets by developing an advanced system for code file enumerations</li>
+                        <li>Proposed and implemented new changes outside the scope of current projects improving debugging and code quality</li>
 			        </ul>
                 </div>
             </div>
 
-            <div className="expirence3">
-                <div className="expirence3-title">
-                    <h1>Agincourt Tutoring</h1>
-                    <h2>Math Tutor</h2>
+            <div className="expirence2" onclick="window.location=http://google.com">
+                <div className="expirence2-title">
+                <a href="https://www.ford.com/"><h1>Ford Motor Company of Canada</h1></a>
+                    <h2>Software Engineering Intern</h2>
                 </div>
-                <div className="expirence3-p">
+                <div className="expirence2-p">
                     <ul>
-                        <li>Tutored grade 9, 10 and 11 students and gave them useful study tips</li>
-				        <li>Effectively communicated with students to complete challenging problems, and helped students gain intensive knowledge on mathematical concepts</li>
+                        <li>Developed multiple Android app pages using Android UI, XML, and Kotlin for Fords in car infotainment system</li>
+				        <li>Migrated XML components to the new JetPack Compose framework reducing errors and increasing testing speed</li>
+                        <li>Actively participated in bi-weekly planning meetings with team leaders and product owners to improve team performance</li>
 			        </ul>
                 </div>
             </div>
