745409732915b6037cd6bc3fafb54c65d8eb30b6
Nilay0921 <61914014+Nilay0921@users.noreply.github.com>
2022-10-10 01:58:56 -0400

    Fixed formatting bug

--- src/components/exp.js
+++ src/components/exp.js
@@ -7,21 +7,6 @@ function exp() {
             <div className="expirence-title">
                 <h1>Experience</h1>
             </div>
-            <div className="expirence2" onclick="window.location=http://google.com">
-                <div className="expirence2-title">
-                    <a href="https://www.fispan.com/"><h1>FISPAN</h1></a>
-                    <h2>Software Developer Intern</h2>
-                </div>
-                <div className="expirence2-p">
-                    <ul>
-                        <li>Worked closely with large-scale microservices architecture to develop new invoice receivables features using Java and Spring impacting 200+ businesses and banks</li>
-                        <li>Improved storage of account information by creating monthly partitions of the SQL table enhancing query processing by 30%</li>
-                        <li>Used Kotlin, J-Unit, and Spring to create rigorous unit tests for production features reducing bugs by 40%</li>
-                        <li>Added new transaction details to the UI using React Js allowing smoother access to transaction information</li>
-			        </ul>
-                </div>
-            </div>
-
             <div className="expirence1" onclick="window.location=http://google.com">
                 <div className="expirence1-title">
                 <a href="https://www.meta.com/"><h1>Meta</h1></a>
@@ -47,6 +32,21 @@ function exp() {
                         <li>Developed multiple Android app pages using Android UI, XML, and Kotlin for Fords in car infotainment system</li>
 				        <li>Migrated XML components to the new JetPack Compose framework reducing errors and increasing testing speed</li>
                         <li>Actively participated in bi-weekly planning meetings with team leaders and product owners to improve team performance</li>
+			        </ul>
+                </div>
+            </div>
+
+            <div className="expirence3" onclick="window.location=http://google.com">
+                <div className="expirence3-title">
+                    <a href="https://www.fispan.com/"><h1>FISPAN</h1></a>
+                    <h2>Software Developer Intern</h2>
+                </div>
+                <div className="expirence3-p">
+                    <ul>
+                        <li>Worked closely with large-scale microservices architecture to develop new invoice receivables features using Java and Spring impacting 200+ businesses and banks</li>
+                        <li>Improved storage of account information by creating monthly partitions of the SQL table enhancing query processing by 30%</li>
+                        <li>Used Kotlin, J-Unit, and Spring to create rigorous unit tests for production features reducing bugs by 40%</li>
+                        <li>Added new transaction details to the UI using React Js allowing smoother access to transaction information</li>
 			        </ul>
                 </div>
             </div>
